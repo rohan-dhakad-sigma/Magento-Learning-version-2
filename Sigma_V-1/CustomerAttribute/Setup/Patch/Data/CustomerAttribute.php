b05bd960740e98dfcfd54af68ef01eb258a2cfd6
rohan-dhakad-sigma <109514052+rohan-dhakad-sigma@users.noreply.github.com>
2023-01-16 19:26:49 +0530

    Sigma_V-1/CreateCustomerAccountGraphQl
    Sigma_V-1/CreateCustomerAccountGraphQl

---
+++ Sigma_V-1/CustomerAttribute/Setup/Patch/Data/CustomerAttribute.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Sigma\CustomerAttribute\Setup\Patch\Data;
+
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Customer\Setup\CustomerSetup;
+
+class CustomerAttribute implements DataPatchInterface, PatchRevertableInterface
+{
+
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+    /**
+     * @var CustomerSetup
+     */
+    private $customerSetupFactory;
+
+    /**
+     * Constructor
+     *
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param CustomerSetupFactory $customerSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        CustomerSetupFactory $customerSetupFactory
+    )
+    {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->customerSetupFactory = $customerSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        /** @var CustomerSetup $customerSetup */
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $customerSetup->addAttribute(
+            \Magento\Customer\Model\Customer::ENTITY,
+            'customer_number', //attribute name
+            [
+                'type' => 'varchar',
+                'label' => 'Customer Number',
+                'input' => 'text',
+                'source' => '',
+                'required' => false,
+                'visible' => true,
+                'position' => 500,
+                'system' => false,
+                'backend' => ''
+            ]
+        );
+
+        $attribute = $customerSetup->getEavConfig()->getAttribute('customer', 'customer_number')->addData([
+            'used_in_forms' => [
+                'adminhtml_checkout',
+                'adminhtml_customer',
+                'adminhtml_customer_address',
+                'customer_account_edit',
+                'customer_address_edit',
+                'customer_register_address',
+                'customer_account_create'
+            ]
+        ]);
+        $attribute->save();
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    public function revert()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        /** @var CustomerSetup $customerSetup */
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $customerSetup->removeAttribute(\Magento\Customer\Model\Customer::ENTITY, 'phone_number');
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [
+
+        ];
+    }
+}
